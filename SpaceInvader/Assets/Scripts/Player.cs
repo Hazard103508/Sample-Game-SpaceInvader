7c22cfea90983a6079eecaaca623632bb45854dc
Agustin Rosso <60651699+Hazard103508@users.noreply.github.com>
2021-11-07 14:42:09 -0300

    colicion con balas enemigas
    se agrega destruccion de la nave del jugador al colicionar con las balas de las naves enemigas

--- SpaceInvader/Assets/Scripts/Player.cs
+++ SpaceInvader/Assets/Scripts/Player.cs
@@ -7,7 +7,9 @@ public class Player : MonoBehaviour
 {
     #region Objects
     private Weapon weapon;
+    private Animator animator;
     private Vector2 screenBounds;
+    private PlayerState playerState;
 
     public float speed;
     #endregion
@@ -16,18 +18,29 @@ public class Player : MonoBehaviour
     void Start()
     {
         weapon = GetComponent<Weapon>();
+        animator = GetComponent<Animator>();
+
         var spriteSize = GetComponent<SpriteRenderer>().sprite.bounds.size;
         this.screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z)) - new Vector3(spriteSize.x / 2, spriteSize.y / 2);
     }
     void Update()
     {
-        Move();
-        Shoot();
+        if (playerState == PlayerState.Normal)
+        {
+            Move();
+            Shoot();
+        }
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Enemy"))
-            print("Die");
+        if (playerState != PlayerState.Dying)
+        {
+            if (collision.CompareTag("Enemy") || collision.CompareTag("Enemy_Bullet"))
+            {
+                playerState = PlayerState.Dying;
+                animator.SetTrigger("Destroy");
+            }
+        }
     }
     #endregion
 
@@ -46,12 +59,27 @@ public class Player : MonoBehaviour
             this.transform.position = new Vector3(this.screenBounds.x, this.transform.position.y, 0);
     }
     /// <summary>
+    /// Al completarse la animacion y desaparecer la nave se detruye el objeto
+    /// </summary>
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+    /// <summary>
     /// Dispara una bala
     /// </summary>
     private void Shoot()
     {
         if (Input.GetKeyDown(KeyCode.Space))
             weapon.Shoot();
+    }
+    #endregion
+
+    #region Structures
+    public enum PlayerState
+    {
+        Normal,
+        Dying
     }
     #endregion
 }
